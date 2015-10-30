d061e971ee6f21ba124a626e90f4537bc02620c2
Mynor Alexander <sinrart@yahoo.com>
2015-10-29 19:00:03 -0700

    [UPDATE] project dev dependencies now use CDN links

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 var videoApp = angular.module('videoApp', []);
+
+videoApp.controller('VideoController', ['$scope', '$window', function($scope, $window) {
+  $scope.videoDisplay = document.getElementById("videoElement");
+  $scope.videoSource = $window.videoSource;
+  $scope.titleDisplay = $window.titleDisplay;
+  $scope.videoDescription = $window.videoDescription;
+}]);
